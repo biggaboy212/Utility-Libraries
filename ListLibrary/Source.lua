1946c14683ac01d8d679adc29402ed81fb1c3dc6
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-01-13 14:45:25 -0600

    Update Source.lua

--- ListLibrary/Source.lua
+++ ListLibrary/Source.lua
@@ -40,6 +40,7 @@ local function EnableDragging(Frame)
 	end)
 end
 
+local ListFunctions = {}
 
 function ListLibrary:CreateList(Name)
 local Name = Name or 'Title'
@@ -66,7 +67,7 @@ ListDictionary["_ListGui"].IgnoreGuiInset = true
 ListDictionary["_ListGui"].ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
 ListDictionary["_ListGui"].ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 ListDictionary["_ListGui"].Name = NewName
-ListDictionary["_ListGui"].Parent = game:GetService("Players").LocalPlayer.PlayerGui
+ListDictionary["_ListGui"].Parent = game:GetService("CoreGui")
 ListParent = ListDictionary["_ListGui"]
 
 ListDictionary["_List"].AnchorPoint = Vector2.new(0.5, 0.5)
@@ -190,6 +191,14 @@ ListDictionary["_UnclipTOP"].Size = UDim2.new(0, 0, 0, -3)
 ListDictionary["_UnclipTOP"].Name = "UnclipTOP"
 ListDictionary["_UnclipTOP"].Parent = ListDictionary["_ListScrollingFrame"]
 	
+function ListFunctions:ClearList()
+	for i,v in pairs(ListDictionary["_ListScrollingFrame"]:GetChildren()) do
+		if v:IsA("TextButton") then
+			v:Destroy()
+		end
+	end
+end
+	
 	local Debounce = false
 	
 	local function StartDebounce()
@@ -222,24 +231,22 @@ ListDictionary["_UnclipTOP"].Parent = ListDictionary["_ListScrollingFrame"]
 		game:GetService('TweenService'):Create(ListDictionary["_List"], TweenInfo.new(0.5, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 243,0, 0)}):Play()
 		task.wait(0.5)
 		ListDictionary["_ListGui"]:Destroy()
-	end)
-	
-end
-
-function ListLibrary:CreateListButton(Name, Callback)
-	wait()
+		end)
+		
+	function ListFunctions:CreateListButton(Name, Effects, Callback)
+	task.wait(0)
 	local Name = Name or 'Title'
 	local Callback = Callback or function() end
-	
+
 	local Button = {
 		["_Copy"] = Instance.new("TextButton");
 		["_UICorner2"] = Instance.new("UICorner");
 		["_UIStroke"] = Instance.new("UIStroke");
 	}
-	
+
 	Button["_Copy"].Font = Enum.Font.GothamMedium
 	Button["_Copy"].Text = Name
-	Button["_Copy"].TextColor3 = Color3.fromRGB(240.00000089406967, 240.00000089406967, 240.00000089406967)
+	Button["_Copy"].TextColor3 = Color3.fromRGB(240, 240, 240)
 	Button["_Copy"].TextSize = 11
 	Button["_Copy"].TextTransparency = 0.10000000149011612
 	Button["_Copy"].AutoButtonColor = false
@@ -248,8 +255,8 @@ function ListLibrary:CreateListButton(Name, Callback)
 	Button["_Copy"].BorderSizePixel = 0
 	Button["_Copy"].Position = UDim2.new(0.0241228063, 0, 0.0181159414, 0)
 	Button["_Copy"].Size = UDim2.new(0, 209, 0, 23)
-	Button["_Copy"].Name = "Copy"
-	Button["_Copy"].Parent = ListParent:WaitForChild("List"):WaitForChild("ListFrame"):WaitForChild("ListScrollingFrame")
+	Button["_Copy"].Name = Name
+	Button["_Copy"].Parent = ListDictionary["_ListScrollingFrame"]
 
 	Button["_UICorner2"].Parent = Button["_Copy"]
 
@@ -257,6 +264,7 @@ function ListLibrary:CreateListButton(Name, Callback)
 	Button["_UIStroke"].Color = Color3.fromRGB(45.00000111758709, 45.00000111758709, 45.00000111758709)
 	Button["_UIStroke"].Parent = Button["_Copy"]
 	
+
 	Button["_Copy"].MouseButton1Click:Connect(function()
 		if not Minimized then
 			task.spawn(function()
@@ -264,6 +272,132 @@ function ListLibrary:CreateListButton(Name, Callback)
 			end)
 		end
 	end)
-end
 
+	function Button:UpdateButton(NewName)
+		Button["_Copy"].Text = NewName
+		Button["_Copy"].Name = NewName
+	end
+	
+		-- i have no clue where i got this from but it's stolen from one of my old UI's
+		if Effects then
+		local btn = Button["_Copy"]
+
+		local TweenService = game:GetService("TweenService")
+
+		local Setting_EnableMask = true -- If true then the ripple effect will be given a mask.
+		local Setting_Color = Color3.fromRGB(0,0,0) -- Sets the color of the object when selected.
+		local Setting_xoffset = 0 -- The offset of the ripple effect in the X-Axis.
+		local Setting_yoffset = -36 -- The offset of the ripple effect in the Y-Axis.
+		local Setting_Lifetime = 0.5 -- The lifetime of the ripple effect.
+		local Setting_Scale = 100 -- The scale of the ripple effect.
+		local Setting_RippleAlpha = 0.5-- The start transparency of the effect.
+		local Setting_SelectionAlpha = 0.75-- The start transparency of the effect.
+
+		local down = false -- If the button is down.
+
+		local dc = Instance.new("Frame")
+		dc.Name = "MaterialSelectedColor"
+		dc.Size = UDim2.new(1,0,1,0)
+		dc.BorderSizePixel = 0
+		dc.BackgroundColor3 = Setting_Color
+		dc.BackgroundTransparency = 1
+		dc.ZIndex = btn.ZIndex
+		dc.Visible = Setting_EnableMask
+		dc.Parent = btn
+
+		local dc_colorguitween = Instance.new("Frame")
+		dc_colorguitween.Visible = false
+		dc_colorguitween.Name = "ColorTween"
+		dc_colorguitween.Position = UDim2.new(1,0,0,0)
+		dc_colorguitween.Parent = dc
+
+		dc_colorguitween.Changed:connect(function()
+			dc.BackgroundTransparency = dc_colorguitween.Position.X.Scale
+		end)
+
+		local rippletweengoal = {}
+		rippletweengoal.ImageTransparency = 1
+
+		local maskf = Instance.new("Frame")
+		maskf.Name = "RippleMask"
+		maskf.Size = UDim2.new(1,0,1,0)
+		maskf.ClipsDescendants = true
+		maskf.BackgroundTransparency = 1
+		maskf.Parent = btn
+
+		local upevent = Instance.new("BindableEvent")
+		upevent.Name = "MouseButton1UpEvent"
+
+		local function ripplemask(x,y)
+			local a = Instance.new("ImageLabel")
+			a.Name = "MaterialRipple"
+			a.Position = UDim2.new(0,x+Setting_xoffset-btn.AbsolutePosition.X,0,y+Setting_yoffset-btn.AbsolutePosition.Y)
+			a.Size = UDim2.new(0,0,0,0)
+			a.BackgroundTransparency = 1
+			a.Image = "rbxasset://textures/whiteCircle.png"
+			a.ImageColor3 = Setting_Color
+			a.ImageTransparency = Setting_RippleAlpha
+			a.AnchorPoint = Vector2.new(0.5,0.5)
+			a.ZIndex = btn.ZIndex
+			a.Parent = maskf
+			a:TweenSize(UDim2.new(0,Setting_Scale,0,Setting_Scale),Enum.EasingDirection.Out,Enum.EasingStyle.Sine,Setting_Lifetime,true)
+			--a.ZIndex = btn.ZIndex-1
+			--a.Parent = btn
+			--a:TweenSizeAndPosition(UDim2.new(0,Setting_Scale,0,Setting_Scale),UDim2.new(0.5,0,0.5,0),Enum.EasingDirection.Out,Enum.EasingStyle.Sine,Setting_Lifetime,true)
+			local tween = TweenService:Create(a,TweenInfo.new(Setting_Lifetime),rippletweengoal)
+			tween:Play()
+			task.wait(Setting_Lifetime)
+			a:Destroy()
+		end
+
+			local function ripple(x,y)
+				local a = Instance.new("ImageLabel")
+				a.Name = "MaterialRipple"
+				a.Position = UDim2.new(0,x+Setting_xoffset-btn.AbsolutePosition.X,0,y+Setting_yoffset-btn.AbsolutePosition.Y)
+				a.Size = UDim2.new(0,0,0,0)
+				a.BackgroundTransparency = 1
+				a.Image = "rbxasset://textures/whiteCircle.png"
+				a.ImageColor3 = Setting_Color
+				a.ImageTransparency = Setting_RippleAlpha
+				a.AnchorPoint = Vector2.new(0.5,0.5)
+				a.ZIndex = btn.ZIndex-1
+				a.Parent = btn
+				a:TweenSizeAndPosition(UDim2.new(0,Setting_Scale,0,Setting_Scale),UDim2.new(0.5,0,0.5,0),Enum.EasingDirection.Out,Enum.EasingStyle.Sine,Setting_Lifetime,true)
+				upevent.Event:connect(function()
+					local tween = TweenService:Create(a,TweenInfo.new(Setting_Lifetime),rippletweengoal)
+					tween:Play()
+					task.wait(Setting_Lifetime)
+					a:Destroy()
+				end)
+			end
+
+			btn.MouseButton1Down:connect(function(x,y)
+				down = true
+				dc_colorguitween:TweenPosition(UDim2.new(Setting_SelectionAlpha,0,0,0),Enum.EasingDirection.Out,Enum.EasingStyle.Linear,0.75,true)
+				if Setting_EnableMask ~= true then
+					ripple(x,y)
+				end
+			end)
+
+			btn.MouseButton1Up:connect(function(x,y)
+				if down == true then
+					down = false
+					dc_colorguitween:TweenPosition(UDim2.new(1,0,0,0),Enum.EasingDirection.In,Enum.EasingStyle.Linear,0.3,true)
+					if Setting_EnableMask == true then
+						ripplemask(x,y)
+					end
+				end
+				upevent:Fire()
+			end)
+
+			btn.MouseLeave:connect(function(x,y)
+				down = false
+				dc_colorguitween:TweenPosition(UDim2.new(1,0,0,0),Enum.EasingDirection.In,Enum.EasingStyle.Linear,0.3,true)
+				upevent:Fire()
+			end)
+		end
+		return Button
+	end
+	return ListFunctions
+end
 return ListLibrary
